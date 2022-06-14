082205c97728bce9d08f27375bca6101ff2fa482
BrunoDevF <brunohenriquex00@gmail.com>
2022-06-13 22:45:32 -0300

    utilizando font personalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-
+import styles from '../styles/home.module.scss'
 export default function Home() {
   return (
-    <div>
+    <div className={styles.title}>
       <h1>Hello world</h1>
     </div>
   )
