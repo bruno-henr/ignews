23afb8d2f879c16961b4537333d1b221b5496ace
BrunoDevF <brunohenriquex00@gmail.com>
2022-06-25 21:50:18 -0300

    feat: verificando se usuario possui uma sessao

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession  } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export const SignInButton: React.FC = () => {
-    const isUserLoggedIn = true;
+    const { data }  = useSession ();
 
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton}>
+    console.log('session', data)
+
+    return data != null ? (
+        <button
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
             <FaGithub color="#04d361" />
-            Bruno Henr.
+            {data.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton}>
+        <button
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417" />
             Sign in with github
         </button>
