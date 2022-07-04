7ee42ee10079114b6ddcb5883b5004a46e8100cf
BrunoDevF <brunohenriquex00@gmail.com>
2022-07-04 15:23:59 -0300

    feat: passando funcao de subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import { useSession, signIn } from 'next-auth/react'
+
+import { api } from '../../services/api'
 
 import styles from './styles.module.scss'
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export const SubscribeButton: React.FC = ({ priceId }: SubscribeButtonProps) => {
+  const { data } = useSession();
+
+  const handleSubscribe = async () => {
+    if (!data) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+
+  }
+
   return (
-    <button className={styles.subscribeButton}>
-        Subscribe now
+    <button
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
     </button>
   )
 }
