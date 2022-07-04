02670d7de75466be7e0b9e6b77247ca950a2912f
BrunoDevF <brunohenriquex00@gmail.com>
2022-07-04 19:03:02 -0300

    feat: manipulando mais eventos do stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -41,18 +43,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         const { type } = event
 
-        console.log('typeee', type);
-        console.log('relevantEvents', relevantEvents);
 
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+
+                        const subscription = event.data.object as Stripe.subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false,
+                        )
+
+                        break;
                     case 'checkout.session.completed':
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                         )
                         break;
                     default:
