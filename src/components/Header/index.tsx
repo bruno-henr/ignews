9006ec954b5aea3a39f9b54a482e8797745e2f2c
bruno_henr <brunohenriquex0@gmail.com>
2022-07-26 20:15:22 -0300

    feat: adicionado endereco de pagina no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,14 @@ import { SignInButton } from '../SignInButton/index';
 import styles from './styles.module.scss'
 
 export const Header: React.FC = () => {
+
   return (
     <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
             <img src="/images/logo.svg" alt="ig.news" />
             <nav>
-                <a className={styles.active}>Home</a>
-                <a>Posts</a>
+                <a href='/' className={styles.active}>Home</a>
+                <a href='/posts' >Posts</a>
             </nav>
 
             <SignInButton />
